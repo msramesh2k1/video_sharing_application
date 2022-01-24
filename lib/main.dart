610df40d6670cd005879f393a06d1f5bf22e1545
Ramesh M S <msramesh2k1@gmail.com>
2022-01-24 12:42:37 +0530

    feat:Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
